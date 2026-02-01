22b25d54ee7a4168e4092e8f52e636b138318a07
DDDenis <61794142+Denis-Viorel@users.noreply.github.com>
2026-02-01 13:04:30 +0200

    commit with fixes for gh-pages

--- src/OneLittleQuestion.tsx
+++ src/OneLittleQuestion.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import backgroundUrl from "./assets/background.svg";
 import gifMain from "./assets/gif-main.gif";
--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/one-little-question/",
   plugins: [react(), tailwindcss(), svgr(),],
 })
